9af1ccd47b4054f8ec9360bc84547217408971a2
naopen <naokizssm@gmail.com>
2024-04-20 01:35:15 +0900

    Add plot_pixel function to draw pixels efficiently

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,11 +6,20 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 19:12:45 by nkannan           #+#    #+#             */
-/*   Updated: 2024/04/20 01:14:58 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/04/20 01:27:45 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
+
+void	plot_pixel(t_fractol *fractol, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = fractol->image.pixels + (y * fractol->image.line_size + x
+			* (fractol->image.bpp / 8));
+	*(unsigned int *)dst = color;
+}
 
 static void	draw_mandelbrot(t_fractol *fractol, int x, int y)
 {
