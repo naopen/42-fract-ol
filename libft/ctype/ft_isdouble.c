4f115cc83635388f4b693e6c3ca2d8869aa8f8dd
naopen <naokizssm@gmail.com>
2024-05-03 19:38:08 +0900

    Refactor ft_isdouble to improve readability and maintainability

--- libft/ctype/ft_isdouble.c
+++ libft/ctype/ft_isdouble.c
@@ -6,38 +6,44 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 23:47:58 by nkannan           #+#    #+#             */
-/*   Updated: 2024/05/03 19:28:26 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/05/03 19:35:55 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-bool	ft_isdouble(char *str)
+static bool	check_format(char *str, bool *is_dot, bool *has_digit)
 {
 	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '.')
+		{
+			if (*is_dot)
+				return (false);
+			*is_dot = true;
+		}
+		else if (ft_isdigit(str[i]))
+			*has_digit = true;
+		else
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+bool	ft_isdouble(char *str)
+{
 	bool	is_dot;
 	bool	has_digit;
 
 	is_dot = false;
 	has_digit = false;
-	i = 0;
 	if (str == NULL || *str == '\0')
 		return (false);
 	if (*str == '-' || *str == '+')
 		str++;
-	while (str[i])
-	{
-		if (str[i] == '.')
-		{
-			if (is_dot) // 小数点が2度目に現れた場合
-				return (false);
-			is_dot = true;
-		}
-		else if (ft_isdigit(str[i]))
-			has_digit = true; // 数値部分が少なくとも1つは存在する
-		else
-			return (false); // 数値でも小数点でもない文字があった場合
-		i++;
-	}
-	return (has_digit); // 数値部分が1つもない場合はfalseを返す
+	return (check_format(str, &is_dot, &has_digit) && has_digit);
 }
